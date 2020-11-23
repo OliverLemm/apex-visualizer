8943dbbfbd9158756c32668b43a46a70076eada0
olemm <oliver.lemm@mt-ag.com>
2020-11-23 23:04:00 +0100

    new subfolder for the APEX app file

---
+++ export_apex_app_sqlcl.bat
@@ -0,0 +1,3 @@
+@echo on
+sql apex_visualizer/apex_visualizer1@orclpdb @export_apex_app_sqlcl 347
+sql apex_visualizer/apex_visualizer1@orclpdb @export_apex_app_sqlcl 347 -split
---
+++ export_apex_app_sqlcl.sql
@@ -0,0 +1,2 @@
+PROMPT Beispiel: apex export -applicationid 1000 -split -skipExportDate
+apex export -applicationid &1 &2 -skipExportDate
