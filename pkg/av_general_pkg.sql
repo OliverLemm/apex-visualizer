ed0bffe077e2fa0dea0f8a7f384434d2f7b2651e
Oliver Lemm <oliver.lemm@mt-ag.com>
2021-09-27 17:43:35 +0200

    added feature to extract target page from link

--- pkg/av_general_pkg.sql
+++ pkg/av_general_pkg.sql
@@ -14,6 +14,12 @@ create or replace package av_general_pkg is
    ,pi_open_in_new_page in number default 1
   ) return varchar2;
 
+  function f_get_page_id_from_target_link
+  (
+    pi_target_link                 in varchar2
+   ,pi_page_id_when_target_is_null in number default null
+  ) return number;
+
 end av_general_pkg;
 /
 create or replace package body av_general_pkg is
@@ -43,6 +49,33 @@ create or replace package body av_general_pkg is
     return '<a href="' || f_get_page_designer_url(pi_app_id      => pi_app_id
                                                  ,pi_app_page_id => pi_app_page_id) || '" ' || case when pi_open_in_new_page = 1 then 'target="blank"' else '' end || '>' || pi_link_label || '</a>';
   end f_get_page_designer_link;
+
+  -- %param pi_target_link f?p= APP_ID.:578: SESSION.:: DEBUG.:::
+  -- %param pi_page_id 
+  function f_get_page_id_from_target_link
+  (
+    pi_target_link                 in varchar2
+   ,pi_page_id_when_target_is_null in number default null
+  ) return number is
+  
+  begin
+    if pi_target_link is null
+    then
+      return pi_page_id_when_target_is_null;
+    else
+      return to_number( -- replace APP_PAGE_ID with own page_id
+                       replace(
+                               -- pi_target_link is like f?p= APP_ID.:578: SESSION.:: DEBUG.:::
+                               -- 578 has to be filtered for link target
+                               substr(pi_target_link
+                                     ,14
+                                     ,instr(substr(pi_target_link
+                                                  ,14)
+                                           ,':') - 1)
+                              ,'&' || 'APP_PAGE_ID.'
+                              ,pi_page_id_when_target_is_null));
+    end if;
+  end f_get_page_id_from_target_link;
 
 end av_general_pkg;
 /
