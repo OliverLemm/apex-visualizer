11f32bdbee31393325e7fe1352fdbb27d8adf7c5
Oliver Lemm <oliver.lemm@mt-ag.com>
2025-07-27 17:30:32 +0200

    APEX Instanz auf 24.2.7 aktualisiert

--- app/f347/application/set_environment.sql
+++ app/f347/application/set_environment.sql
@@ -19,7 +19,7 @@ whenever sqlerror exit sql.sqlcode rollback
 begin
 wwv_flow_imp.import_begin (
  p_version_yyyy_mm_dd=>'2024.11.30'
-,p_release=>'24.2.3'
+,p_release=>'24.2.7'
 ,p_default_workspace_id=>100001
 ,p_default_application_id=>347
 ,p_default_id_offset=>20408574139881448
